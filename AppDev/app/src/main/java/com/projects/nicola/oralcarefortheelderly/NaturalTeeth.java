3d14c83caa8cd990af75f79289142c7e40303e3b
Nicola <c15755031@mydit.ie>
2017-04-13 16:49:37 +0100

    added and tested new (natural teeth) parent view to child (cavities)... works

--- AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/NaturalTeeth.java
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/NaturalTeeth.java
@@ -1,5 +1,6 @@
 package com.projects.nicola.oralcarefortheelderly;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,14 +17,13 @@ public class NaturalTeeth extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+    }
+
+    public void gotoCavity(View view)
+    {
+        Intent newIntent = new Intent(this, cavities.class);
+        startActivity(newIntent);
     }
 
 }

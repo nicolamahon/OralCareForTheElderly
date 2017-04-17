91ca25b2767976b776be485278d9b41bef1da6ba
Nicola <c15755031@mydit.ie>
2017-04-17 22:52:54 +0100

    added main Gum Disease activity; children activities to follow

---
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/GumDiseaseMain.java
@@ -0,0 +1,30 @@
+package com.projects.nicola.oralcarefortheelderly;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class GumDiseaseMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gum_disease_main);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public void gotoGumSigns(View view)
+    {
+        Intent newIntent = new Intent(this, Hygiene.class);
+        startActivity(newIntent);
+    }
+
+    public void gotoGumCauses(View view)
+    {
+        Intent newIntent = new Intent(this, CareForDenture.class);
+        startActivity(newIntent);
+    }
+
+}

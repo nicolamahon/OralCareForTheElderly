14641dc038d52e8205be7f980e5d7ab926814fc2
AaronAnthonyByrne <C15709609@mydit.ie>
2017-04-19 16:22:43 +0100

    Finished Register activity, included error checking for passwords.

--- AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/Register.java
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/Register.java
@@ -1,11 +1,14 @@
 package com.projects.nicola.oralcarefortheelderly;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
@@ -15,6 +18,27 @@ public class Register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
 
+    }
+
+    public void confirmRegister(View view)
+    {
+        EditText name = (EditText) findViewById(R.id.Name_registered);
+        EditText email = (EditText) findViewById(R.id.Email_registered);
+        EditText uname = (EditText) findViewById(R.id.Username_registered);
+        EditText pass1 = (EditText) findViewById(R.id.pass1_registered);
+        EditText pass2 = (EditText) findViewById(R.id.pass2_registered);
+
+        String namestr = name.getText().toString();
+        String eamilstr = email.getText().toString();
+        String unamestr = uname.getText().toString();
+        String pass1str = pass1.getText().toString();
+        String pass2str = pass2.getText().toString();
+
+        if(!pass1str.equals(pass2str))
+        {
+            Toast passError = Toast.makeText(this, "Passwords do not match!",Toast.LENGTH_SHORT);
+            passError.show();
+        }
     }
 }
 

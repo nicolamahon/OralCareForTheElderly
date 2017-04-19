7b9970fc9d986d421c8b04ab19f314f026c32c93
Nicola <c15755031@mydit.ie>
2017-04-19 15:53:49 +0100

    added Dry Mouth Remedies activity; testing and refactoring needed, button push not linked yet

--- AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/DryMouth.java
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/DryMouth.java
@@ -1,7 +1,9 @@
 package com.projects.nicola.oralcarefortheelderly;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DryMouth extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class DryMouth extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dry_mouth);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+    }
+
+    public void gotoDryRemedies(View view)
+    {
+        Intent newIntent = new Intent(this, DryMouthRemedies.class);
+        startActivity(newIntent);
     }
 }
--- AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/DryMouthRemedies.java
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/DryMouthRemedies.java
@@ -1,11 +1,7 @@
 package com.projects.nicola.oralcarefortheelderly;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class DryMouthRemedies extends AppCompatActivity {
 
@@ -13,17 +9,8 @@ public class DryMouthRemedies extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dry_mouth_remedies);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
     }
 
 }

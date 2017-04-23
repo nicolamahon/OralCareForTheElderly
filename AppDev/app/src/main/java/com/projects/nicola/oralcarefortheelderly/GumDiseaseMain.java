18d98baf29d6573e8911f6ea258377549f156b72
Nicola <c15755031@mydit.ie>
2017-04-23 21:58:24 +0100

    Deleted obsolete files for Gum Disease Signs/Causes: java, content.XML, activities.XML and removed activities from Manifest.XML

--- AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/GumDiseaseMain.java
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/GumDiseaseMain.java
@@ -1,10 +1,8 @@
 package com.projects.nicola.oralcarefortheelderly;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class GumDiseaseMain extends AppCompatActivity {
 
@@ -15,7 +13,6 @@ public class GumDiseaseMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gum_disease_main);
-        //getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
 
         viewPager = (ViewPager)findViewById(R.id.view_pager_gumdisease);
         adapter = new GumDiseaseSwipeAdapter(this);
@@ -23,18 +20,4 @@ public class GumDiseaseMain extends AppCompatActivity {
 
     }
 
-    /*
-    public void gotoGumSigns(View view)
-    {
-        Intent newIntent = new Intent(this, GumDiseaseSigns.class);
-        startActivity(newIntent);
-    }
-
-
-    public void gotoGumCauses(View view)
-    {
-        Intent newIntent = new Intent(this, GumDiseaseCauses.class);
-        startActivity(newIntent);
-    }
-    */
 }

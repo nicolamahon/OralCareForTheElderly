b0320b9fd9eaf4f29ef5f74ba5f37d3c1a43a986
AaronAnthonyByrne <C15709609@mydit.ie>
2017-04-24 16:19:54 +0100

    Started on the Patient care section

--- AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/patient_care.java
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/patient_care.java
@@ -1,7 +1,9 @@
 package com.projects.nicola.oralcarefortheelderly;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class patient_care extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class patient_care extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_care);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+    }
+
+    public void gotoResidents(View view)
+    {
+        Intent intent = new Intent(this, Resident.class);
+        startActivity(intent);
+    }
+
+    public void gotoBroken(View view)
+    {
+        Intent intent = new Intent(this, Broken_Denture.class);
+        startActivity(intent);
+    }
+
+    public void gotoLost(View view)
+    {
+        Intent intent = new Intent(this, Lost_Denture.class);
+        startActivity(intent);
+    }
+
+    public void gotoEmergancy(View view)
+    {
+        Intent intent = new Intent(this, Emergancy.class);
+        startActivity(intent);
     }
 }

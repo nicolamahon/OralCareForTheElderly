c5dc240e1e30577870c66b7e6ca1e570159db565
Nicola <c15755031@mydit.ie>
2017-04-25 11:26:30 +0100

    FIXED EMERGENCY HERE; emergency activity updated to correct version. Cosmetic changes to Cancer activity.

--- AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/Emergency.java
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/Emergency.java
@@ -1,11 +1,7 @@
 package com.projects.nicola.oralcarefortheelderly;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class Emergency extends AppCompatActivity {
 

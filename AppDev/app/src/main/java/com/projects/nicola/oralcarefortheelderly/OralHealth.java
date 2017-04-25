3845607229398cc75708d8aff7bc5a852c4293c2
Nicola <c15755031@mydit.ie>
2017-04-25 03:27:27 +0100

    Added sub-menus and buttons to Mouth Care activity. New activity Thrush added to app, tested, refactored, cosmetic changes done. COMPLETE

--- AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/OralHealth.java
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/OralHealth.java
@@ -1,7 +1,9 @@
 package com.projects.nicola.oralcarefortheelderly;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class OralHealth extends AppCompatActivity {
 
@@ -11,4 +13,35 @@ public class OralHealth extends AppCompatActivity {
         setContentView(R.layout.activity_oral_health);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
     }
+
+    public void gotoCavity(View view)
+    {
+        Intent newIntent = new Intent(this, cavities.class);
+        startActivity(newIntent);
+    }
+
+    public void gotoPlaque(View view)
+    {
+        Intent newIntent = new Intent(this, Plaque.class);
+        startActivity(newIntent);
+    }
+
+    public void gotoGumDis(View view)
+    {
+        Intent newIntent = new Intent(this, GumDiseaseMain.class);
+        startActivity(newIntent);
+    }
+
+    public void gotoThrush (View view)
+    {
+        Intent newIntent = new Intent(this, Thrush.class);
+        startActivity(newIntent);
+    }
+    /*
+    public void gotoCancer (View view)
+    {
+        Intent newIntent = new Intent(this, Cancer.class);
+        startActivity(newIntent);
+    }
+    */
 }
---
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/Thrush.java
@@ -0,0 +1,18 @@
+package com.projects.nicola.oralcarefortheelderly;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nicola on 2017-04-25.
+ */
+
+public class Thrush extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thrush);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+    }
+}

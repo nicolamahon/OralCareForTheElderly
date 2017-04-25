97077526040421b8b0f5bf294e28fd48d1aa282c
AaronAnthonyByrne <C15709609@mydit.ie>
2017-04-25 15:39:49 +0100

    Moved buttons around, put footer at the end on login page and information page.

--- AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/information.java
+++ AppDev/app/src/main/java/com/projects/nicola/oralcarefortheelderly/information.java
@@ -3,10 +3,86 @@ package com.projects.nicola.oralcarefortheelderly;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
 public class information extends AppCompatActivity {
+
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Mapbox access token is configured here.
+        Mapbox.getInstance(this, getString(R.string.access_token));
+
+        // This contains the MapView in XML and needs to be called after the account manager
         setContentView(R.layout.activity_information);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(53.3069832,-6.3210787))
+                        .title("DentalTech")
+                        .snippet("Call us: 01 4099 106 / 01 4551 866\n Opening hours:\n" +
+                                "8am to 5.30pm Mon/Tues/Fri\n" +
+                                "8am to 8pm Wed/Thurs"));
+
+            }
+        });
+    }
+
+    // Add the mapView lifecycle to the activity's lifecycle methods
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
+
